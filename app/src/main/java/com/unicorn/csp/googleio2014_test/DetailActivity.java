1160b83dddaf27e74a558e7658708ea7243442cf
renjiajian <renjiajian@withub.net.cn>
2015-10-30 17:25:42 +0800

    Initial commit

---
+++ app/src/main/java/com/unicorn/csp/googleio2014_test/DetailActivity.java
@@ -0,0 +1,180 @@
+package com.unicorn.csp.googleio2014_test;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.transition.Transition;
+import android.view.View;
+import android.view.ViewPropertyAnimator;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.ImageView;
+
+import io.codetail.animation.SupportAnimator;
+import io.codetail.animation.ViewAnimationUtils;
+
+/**
+ * Created by Administrator on 2015/10/29.
+ */
+public class DetailActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                View myView = findViewById(R.id.photo);
+
+                // get the center for the clipping circle
+                int cx = (myView.getLeft() + myView.getRight()) / 2;
+                int cy = (myView.getTop() + myView.getBottom()) / 2;
+
+                // get the final radius for the clipping circle
+                int dx = Math.max(cx, myView.getWidth() - cx);
+                int dy = Math.max(cy, myView.getHeight() - cy);
+                float finalRadius = (float) Math.hypot(dx, dy);
+
+                SupportAnimator animator =
+                        ViewAnimationUtils.createCircularReveal(myView, cx, cy, 0, finalRadius);
+                animator.setInterpolator(new AccelerateDecelerateInterpolator());
+                animator.setDuration(1500);
+                animator.start();
+            }
+        });
+
+
+        getWindow().getEnterTransition().addListener(new Transition.TransitionListener() {
+            @Override
+            public void onTransitionStart(Transition transition) {
+                ImageView hero = (ImageView) findViewById(R.id.photo);
+                ObjectAnimator color = ObjectAnimator.ofArgb(hero.getDrawable(), "tint",
+                        getResources().getColor(R.color.photo_tint), 0);
+
+                color.addListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+
+                        findViewById(R.id.fab).animate().setStartDelay(30)
+                                .scaleX(1).scaleY(1).setListener(new Animator.AnimatorListener() {
+                            @Override
+                            public void onAnimationStart(Animator animation) {
+
+                            }
+
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+
+                                findViewById(R.id.title).animate().setStartDelay(30)
+                                        .scaleY(1).setListener(new Animator.AnimatorListener() {
+                                    @Override
+                                    public void onAnimationStart(Animator animation) {
+
+                                    }
+
+                                    @Override
+                                    public void onAnimationEnd(Animator animation) {
+
+                                        findViewById(R.id.description).animate().setStartDelay(30)
+                                                .scaleY(1);
+                                    }
+
+                                    @Override
+                                    public void onAnimationCancel(Animator animation) {
+
+                                    }
+
+                                    @Override
+                                    public void onAnimationRepeat(Animator animation) {
+
+                                    }
+                                });
+
+                            }
+
+                            @Override
+                            public void onAnimationCancel(Animator animation) {
+
+                            }
+
+                            @Override
+                            public void onAnimationRepeat(Animator animation) {
+
+                            }
+                        });
+
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+
+                    }
+                });
+                color.start();
+
+
+                getWindow().getEnterTransition().removeListener(this);
+            }
+
+            @Override
+            public void onTransitionEnd(Transition transition) {
+
+            }
+
+            @Override
+            public void onTransitionCancel(Transition transition) {
+
+            }
+
+            @Override
+            public void onTransitionPause(Transition transition) {
+
+            }
+
+            @Override
+            public void onTransitionResume(Transition transition) {
+
+            }
+        });
+    }
+
+    public ViewPropertyAnimator showViewByScale(View v) {
+
+        ViewPropertyAnimator propertyAnimator = v.animate().setStartDelay(30)
+                .scaleX(1).scaleY(1);
+
+        return propertyAnimator;
+    }
+
+    @Override
+    public void onBackPressed() {
+        ImageView hero = (ImageView) findViewById(R.id.photo);
+        ObjectAnimator color = ObjectAnimator.ofArgb(hero.getDrawable(), "tint",
+                0, getResources().getColor(R.color.photo_tint));
+        color.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                finishAfterTransition();
+            }
+        });
+        color.start();
+
+        findViewById(R.id.fab).animate().alpha(0.0f);
+    }
+
+}
